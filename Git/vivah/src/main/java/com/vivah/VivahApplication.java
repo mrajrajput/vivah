f36e631e7786f5d35265634a2931276e646a97a6
FirstCommit <Manjul@192.168.1.105>
2022-04-11 11:56:23 -0500

    unversioned

---
+++ Git/vivah/src/main/java/com/vivah/VivahApplication.java
@@ -0,0 +1,14 @@
+package com.vivah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VivahApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		SpringApplication.run(VivahApplication.class, args);
+	}
+
+}
